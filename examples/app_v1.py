b5668681f0eb3e6154c0dc10630ae0f60cfdb0ae
trakshan-mishra <trakshanmishra477@gmail.com>
2026-06-14 08:46:36 +0530

    Add examples and sample outputs

---
+++ examples/app_v1.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+def calculate():
+    x = add(2, 3)
+    y = multiply(4, 5)
+    return x + y
+
+def report():
+    return calculate()
